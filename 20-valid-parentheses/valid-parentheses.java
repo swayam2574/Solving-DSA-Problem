8c13c9551edc18daafe00954f1ffb7b43c050484
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-02-15 00:48:23 +0530

    Time: 3 ms (87.52%) | Memory: 43.4 MB (40.94%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,17 +1,40 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(char ch: s.toCharArray()){
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
+        // ______________________ m2
+        if ((s.length() & 1) == 1) return false;
+
+        Deque<Character> dq = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                dq.push(c);
             } else {
-                if(st.isEmpty()) return false;
-                char top = st.peek();
-                if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')) {
-                    st.pop();
-                } else return false;
+                if (dq.isEmpty()) return false;
+                char top = dq.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
             }
         }
-        return st.isEmpty();
+
+        return dq.isEmpty();
+
+
+        // ______________________ m1
+        // Stack<Character> st = new Stack<>();
+        // for(char ch: s.toCharArray()){
+        //     if (ch == '(' || ch == '{' || ch == '[') {
+        //         st.push(ch);
+        //     } else {
+        //         if(st.isEmpty()) return false;
+        //         char top = st.peek();
+        //         if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')) {
+        //             st.pop();
+        //         } else return false;
+        //     }
+        // }
+        // return st.isEmpty();
     }
 }
