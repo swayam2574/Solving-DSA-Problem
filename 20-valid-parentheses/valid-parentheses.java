683a8238e361a1235a9810d00cd24c63b90ff972
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-02-17 00:17:22 +0530

    Time: 3 ms (87.54%) | Memory: 43.1 MB (86.33%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,43 +1,36 @@
 class Solution {
     public boolean isValid(String s) {
         // ______________________ m2
-        int l = s.length();
-        char[] a = new char[l];
-        int top = -1;  
+        // int l = s.length();
+        // char[] a = new char[l];
+        // int top = -1;
 
-        if(l == 1 || l%2 != 0) return false;
+        // if(l==1 || l%2 != 0) return false;
 
-        for (int i=0; i<l; i++) {
-            char c = s.charAt(i);
-
-            if (c == '(' || c == '{' || c == '[') a[++top] = c;
-            else { 
-                if (top == -1) return false;
-
-                char open = a[top--]; 
-
-                if ((c == ')' && open != '(') ||
-                    (c == '}' && open != '{') ||
-                    (c == ']' && open != '[')) {
-                    return false;
-                }
-            }
-        }
-        return top == -1;
+        // for(int i=0; i<l; i++){
+        //     char c = s.charAt(i);
+        //     if(c == '(' || c == '[' || c == '{') a[++top] = c;
+        //     else{
+        //         if(top == -1) return false;
+        //         char p = a[top--];
+        //         if((p != '(' && c == ')') || (p != '[' && c == ']') || (p != '{' && c == '}')) return false;
+        //     }
+        // }
+        // return top == -1;
 
 
         // ______________________ m1
-        // Stack<Character> st = new Stack<>();
-        // for(char ch: s.toCharArray()){
-        //     if (ch == '(' || ch == '{' || ch == '[') {
-        //         st.push(ch);
-        //     } else {
-        //         if(st.isEmpty()) return false;
-        //         char top = st.peek();
-        //         if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')) st.pop();
-        //         else return false;
-        //     }
-        // }
-        // return st.isEmpty();
+        Stack<Character> st = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if(st.isEmpty()) return false;
+                char top = st.peek();
+                if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')) st.pop();
+                else return false;
+            }
+        }
+        return st.isEmpty();
     }
 }
