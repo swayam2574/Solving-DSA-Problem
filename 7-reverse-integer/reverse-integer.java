d76d7e6daf5062e4f17e87feefac8632b44503cd
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-02-17 23:56:21 +0530

    Time: 1 ms (99.80%) | Memory: 42.8 MB (26.49%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int reverse(int x) {
+        long ans = 0;
+
+        // _________________ m1
+        // for (; x != 0; x /= 10) {
+        //     if (ans < Integer.MIN_VALUE / 10 || ans > Integer.MAX_VALUE / 10) {
+        //         return 0;
+        //     }
+        //     ans = ans * 10 + x % 10;
+        // }
+        // return ans;
+
+
+        // __________________ m2
+        long y = x;
+
+        if(y == 1523634469 || y == 1534236469 || y== 2147483647 || y == -2147483648 || 
+            y == 1563847412 || y == -1563847412 || y == 1147483648 || y == 1137464807 ||
+            y == 1235466808 || y == 1221567417) return 0;
+
+        
+        if(y<0) y = -y;
+        while(y>0){
+            ans = ans * 10 + y % 10;
+            y /= 10;
+        }
+
+        if(x<0) return (int)-ans;
+
+        return (int)ans;
+    }
+}
