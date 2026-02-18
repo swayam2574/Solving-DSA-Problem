e30d4cd529099d582bf98a96afed1d932a031f1f
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-02-18 22:51:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (50.87%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -16,12 +16,10 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-
         if(root == null) return ans;
-
         preorder(root, ans);
-
         return ans;
+
     }
     private void preorder(TreeNode root, List<Integer> ans){
         if(root == null) return;
