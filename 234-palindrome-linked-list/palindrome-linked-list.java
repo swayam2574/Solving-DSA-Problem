2de2936211b02f00c8de8eef1358ea7f7cee228b
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-02-21 23:51:28 +0530

    Time: 2 ms (99.75%) | Memory: 96.9 MB (29.31%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,34 +9,64 @@
  * }
  */
 class Solution {
+    static int[] mirror = new int[100000];
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
 
-        ListNode slow = head;
-        ListNode fast = head;
+        // ListNode slow = head;
+        // ListNode fast = head;
 
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        // while (fast != null && fast.next != null) {
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // }
+
+        // ListNode prev = null;
+        // while (slow != null) {
+        //     ListNode next = slow.next;
+        //     slow.next = prev;
+        //     prev = slow;
+        //     slow = next;
+        // }
+
+        // ListNode first = head;
+        // ListNode second = prev;
+
+        // while (second != null) {
+        //     if (first.val != second.val) return false;
+        //     first = first.next;
+        //     second = second.next;
+        // }
+
+        // return true;
+
+        // ____________________________
+        // List<Integer> list = new ArrayList();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     list.add(temp.val);
+        //     temp= temp.next;
+        // }
+        // int l=0, r=list.size()-1;
+        // while(l<r){
+        //     if(list.get(l++)!=list.get(r--)) return false;
+        // }
+        // return true;
+
+
+        // ___________________________
+        int i = 0;
+        ListNode pre = head;
+        while(pre != null){
+            mirror[i++] = pre.val;
+            pre = pre.next;
         }
-
-        ListNode prev = null;
-        while (slow != null) {
-            ListNode next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
+        
+        int l = 0;
+         while (i > 0) {
+            if (mirror[l++] != mirror[--i])
+                return false;
         }
-
-        ListNode first = head;
-        ListNode second = prev;
-
-        while (second != null) {
-            if (first.val != second.val) return false;
-            first = first.next;
-            second = second.next;
-        }
-
         return true;
 
     }
