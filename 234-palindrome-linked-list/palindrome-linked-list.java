58a096d01e7e11533c91d3b44182ec68354d55a2
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-02-24 23:39:53 +0530

    Time: 3 ms (99.72%) | Memory: 96.8 MB (32.12%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -12,30 +12,17 @@ class Solution {
     static int[] arr = new int[100000];
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
-
-        int n = 0;
-        ListNode temp = head;
-
-        while (temp != null) {
-            n++;
-            temp = temp.next;
-        }
-
-        // int[] arr = new int[n];
-        temp = head;
         int i = 0;
-
-        while (temp != null) {
+        ListNode temp = head;
+        while(temp != null){
             arr[i++] = temp.val;
             temp = temp.next;
         }
-
-        int l = 0, r = n - 1;
-
-        while (l < r) {
-            if (arr[l++] != arr[r--]) return false;
+        
+        int l = 0;
+        while (l < i) {
+            if (arr[l++] != arr[--i]) return false;
         }
-
         return true;
 
         // ____________________________ m2
