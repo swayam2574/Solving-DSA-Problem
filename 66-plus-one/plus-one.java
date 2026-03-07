ec0cef677406d5f58d1b51df648e7057679d61d5
Swayam Kumar <144916503+swayam2574@users.noreply.github.com>
2026-03-07 16:55:37 +0530

    Time: 4 ms (1.50%) | Memory: 43.8 MB (6.89%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,20 +1,22 @@
+import java.math.BigInteger;
+
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i=digits.length-1; i>=0; i--){
-            if(digits[i]<9){
-                digits[i]++;
-                return digits;
-            } else if(digits[i]==9){
-                digits[i]=0;
-            }
-        }
+        // _________________________ m1 
+        // for(int i=digits.length-1; i>=0; i--){
+        //     if(digits[i]<9){
+        //         digits[i]++;
+        //         return digits;
+        //     }
+        //     digits[i]=0;
+        // }
 
-        int[] res = new int[digits.length+1];
-        res[0] = 1;
-        return res;
+        // int[] res = new int[digits.length+1];
+        // res[0] = 1;
+        // return res;
 
 
-
+        // _________________________ m2 
         // // fine only for smaller number
         // long n = 0;
         // int p = digits.length-1;
@@ -22,22 +24,22 @@ class Solution {
         //     n+=i*Math.pow(10, p--); 
         // } 
 
-        // ArrayList<Long> al = new ArrayList<>();
-        // long temp = n+1;
-        // while(temp!=0){
-        //     al.add(0, temp%10);
-        //     temp/=10;
-        // } 
+        StringBuilder num = new StringBuilder();
 
-        // StringBuilder sb = new StringBuilder();
-        // for(int i=0; i<al.size(); i++){
-        //     sb.append(al.get(i));
-        // }
-        
-        // int[] res = new int[sb.length()];
-        // for(int i=0; i<res.length; i++){
-        //     res[i] = sb.charAt(i) - '0';
-        // }
-        // return res;
+        for(int i : digits){
+            num.append(i);
+        }
+
+        BigInteger n = new BigInteger(num.toString());
+        n = n.add(BigInteger.ONE);
+
+        String s = n.toString();
+
+        int[] res = new int[s.length()];
+        for(int i = 0; i < s.length(); i++){
+            res[i] = s.charAt(i) - '0';
+        }
+
+        return res;
     }
 }
